918d4a435049199fa20bba062b3a0c9f224cb6ab
JimyTD <604384365@qq.com>
2017-08-31 20:45:20 +0800

    trytest

---
+++ xLua-master/Assets/XLua/Examples/usage Test/UsageTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using XLua;
+
+
+
+
+public class UsageTest : MonoBehaviour {
+
+    // Use this for initialization
+    TextAsset luaScript;
+    LuaTable scriptEnv;
+    Action rotate;
+    Action move;
+    void Start () {
+        LuaEnv luaenv = new LuaEnv();
+        scriptEnv = luaenv.NewTable();
+        scriptEnv.Set("self", this);
+        luaenv.DoString(luaScript.text, "lua",scriptEnv);
+        scriptEnv.Get("rotate", out rotate);
+        scriptEnv.Get("move", out move);
+
+        luaenv.Dispose();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
