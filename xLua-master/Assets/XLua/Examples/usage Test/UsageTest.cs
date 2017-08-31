8555fbcd05652cdf76a8f780fbdb6a102108a889
JimyTD <604384365@qq.com>
2017-08-31 20:56:49 +0800

    2017-08-31 20:58:18

--- xLua-master/Assets/XLua/Examples/usage Test/UsageTest.cs
+++ xLua-master/Assets/XLua/Examples/usage Test/UsageTest.cs
@@ -13,12 +13,25 @@ public class UsageTest : MonoBehaviour {
     Action rotate;
     Action move;
     void Start () {
+        //新建luaEnv:lua全局环境对象；scriptEnv:关联对象
         LuaEnv luaenv = new LuaEnv();
         scriptEnv = luaenv.NewTable();
+
+        //这里是设置元表 指定了索引为Global属性 Global属性在luaenv构造时被设置 
+        LuaTable meta = luaenv.NewTable();
+        meta.Set("__index", luaenv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();//设置完元表是可以销毁的
+
+        //进行关联
         scriptEnv.Set("self", this);
-        luaenv.DoString(luaScript.text, "lua",scriptEnv);
         scriptEnv.Get("rotate", out rotate);
         scriptEnv.Get("move", out move);
+
+        //执行lua脚本
+        luaenv.DoString(luaScript.text, "lua", scriptEnv);
+
+
 
         luaenv.Dispose();
     }
